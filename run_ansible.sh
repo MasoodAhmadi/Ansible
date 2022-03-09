0c93e0fd312e4674d410cc3724f89ea341753209
Masood Ahmadi <masoodahmadi553@gmail.com>
2022-03-09 10:51:50 +0200

    user root and user privilege

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ansible-playbook\
- --ssh-common-args\
-  '-F /home/hostmachine/ansible-demo/ssh/config'\
-    -i /home/hostmachine/ansible-demo/inventory/dev-servers\
-    apache-installation.yml
-
-
-
-#   --extra-vars="deployment_environment=Ubuntu"\
+  --ssh-common-args\
+  '-F config'\
+  -i inventory\
+   --extra-vars='{"deployment_environment":"Ubuntu"}'\
+   --vault-password-file ./myfile.txt\
+   apache-installation.yml
